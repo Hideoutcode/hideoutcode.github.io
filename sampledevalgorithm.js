0931d239a0701726eec937f0d070b8e50de29428
Hack3r5 <115195207+Hideoutcode@users.noreply.github.com>
2023-04-20 13:01:04 -0400

    Create sampledevalgorithm.js

---
+++ sampledevalgorithm.js
@@ -0,0 +1,10 @@
+const string1 = [`1`, `2`, `3`];
+const string2 = [`4`, `5`, `6`]; 
+const string3 = string1.concat(string2);
+
+Document.write(string3)
+
+
+
+var lastElement = myList[MyList.lenght - 1]; 
+console.log(lastElement); 
