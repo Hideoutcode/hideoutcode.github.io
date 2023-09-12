18faae919d31cfcbebe002025810350c0f3e72b3
Hack3r5 <115195207+Hideoutcode@users.noreply.github.com>
2023-09-12 11:16:11 -0400

    Create prototype.js

---
+++ prototype.js
@@ -0,0 +1,34 @@
+ const msgerForm = get(".msger-inputarea");
+const msgerInput = get(".msger-input");
+const msgerChat = get(".msger-chat");
+var Person_Name = "user";
+
+if(var Person_Name = "user";includes("hi, hello, how are you, hola, привет, hallo,"; const BOT_MSGS = [
+  "hi How are You?",
+  "hello",
+  "how can I help you?",
+  "Hi",
+  "Hi, today is a good day!",
+]));
+
+If(var person_name = "user";includes("good", "fine", "a-okay"; const BOT_MSGS = [
+  "How can I help You?",
+  "Anything I can do for you today", 
+  "I am here to help!",
+  ]));
+
+if(var Person_name = "user";includes("i need", "I need", "what is", "is this",; const BOT
+  
+  
+  
+  
+    
+
+
+
+
+const BOT_MSGS = [
+  
+  
+  
+  
