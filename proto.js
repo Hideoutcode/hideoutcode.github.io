63d79b3b0d03b8cbaf8b2ae2756bc02be9984e03
Hack3r5 <115195207+Hideoutcode@users.noreply.github.com>
2023-09-13 12:01:31 -0400

    Update proto.js

--- proto.js
+++ proto.js
@@ -35,37 +35,6 @@ If( Person_name = "user";includes("math";  BOT_DIR = [
  
 ]));
 
-If( BOT_DIR = "math"  myFunction() {
-
-  x=prompt("Type first number");
-  y=prompt("Type second number");
-  oper=prompt("Type an operator");
-  
-  if(oper=="+"){
-    z=Number(x)+Number(y);
-  }
-  else if(oper=!null){
-    z="Error"
-  }
-  else if(x=!null){
-    z="Error"
-  }
-  else if(y=!null){
-    z="Error"
-  }
-  else if(oper=="/"){
-    z=Number(x)/Number(y);
-  }
-  else if(oper=="*"){
-    z=Number(x)*Number(y);
-  }
-  else if(oper=="-"){
-    z=Number(x)-Number(y);
-  }
-  
-  return z;
-    }
-document.getElementById("demo").innerHTML =myFunction());
 
 
 
