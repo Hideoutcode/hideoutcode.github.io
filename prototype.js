2f2c04b55fe80e4b06c6c722940bd57311245c3e
Hack3r5 <115195207+Hideoutcode@users.noreply.github.com>
2023-09-13 10:12:13 -0400

    Update prototype.js

--- prototype.js
+++ prototype.js
@@ -2,6 +2,8 @@
 const msgerInput = get(".msger-input");
 const msgerChat = get(".msger-chat");
 var Person_Name = "user";
+const BOT_DIR = array["math", "code", "back", "start",];
+const BOT_CALC = javascript: 
 
 if(var Person_Name = "user";includes("hi, hello, how are you, hola, привет, hallo,"; const BOT_MSGS = [
   "hi How are You?",
@@ -18,10 +20,57 @@ If(var person_name = "user";includes("good", "fine", "a-okay"; const BOT_MSGS = 
   ]));
 
 if(var Person_name = "user";includes("i need", "I need", "what is", "is this",; const BOT_MSGS = [ 
- "I can help you if you include key words on the subject, that is how my basic javascript ai works not that you probably care you just need a simple answer for right now I am completely irrelavant",]));
+ "I can help you if you include key words on the subject such as math search test or joke, that is how my basic javascript ai works not that you probably care you just need a simple answer for right now I am completely irrelavant",]));
  
 
- const array1 = Array["Math", "math", ];
+ const array1 = Array["Math", "math","search", "test", "Joke"];
+
+If(var Person_name = "user";includes("math"; const BOT_DIR = [
+
+ "math",
+  
+
+ 
+]));
+
+If(const BOT_DIR = "math"; javascript: function myFunction() {
+  var x,y,z,oper;
+  x=prompt("Type first number");
+  y=prompt("Type second number");
+  oper=prompt("Type an operator");
+  
+  if(oper=="+"){
+    z=Number(x)+Number(y);
+  }
+  else if(oper=!null){
+    z="Error"
+  }
+  else if(x=!null){
+    z="Error"
+  }
+  else if(y=!null){
+    z="Error"
+  }
+  else if(oper=="/"){
+    z=Number(x)/Number(y);
+  }
+  else if(oper=="*"){
+    z=Number(x)*Number(y);
+  }
+  else if(oper=="-"){
+    z=Number(x)-Number(y);
+  }
+  
+  return z;
+    }
+document.getElementById("demo").innerHTML =myFunction());
+
+
+
+
+
+
+
 
 
 
@@ -35,6 +84,73 @@ if(var Person_name = "user";includes("i need", "I need", "what is", "is this",; 
 
 
 const BOT_MSGS = [
+
+
+
+
+
+
+
+
+ msgerForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const msgText = msgerInput.value;
+  if (!msgText) return;
+
+  appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
+  msgerInput.value = "";
+
+  botResponse();
+});
+
+function appendMessage(name, img, side, text) {
+  //   Simple solution for small apps
+  const msgHTML = `
+    <div class="msg ${side}-msg">
+      <div class="msg-img" style="background-image: url(${img})"></div>
+
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${name}</div>
+          <div class="msg-info-time">${formatDate(new Date())}</div>
+        </div>
+
+        <div class="msg-text">${text}</div>
+      </div>
+    </div>
+  `;
+
+  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+  msgerChat.scrollTop += 500;
+}
+
+function botResponse() {
+  const r = random(0, BOT_MSGS.length - 1);
+  const msgText = BOT_MSGS[r];
+  const delay = msgText.split(" ").length * 100;
+
+  setTimeout(() => {
+    appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
+  }, delay);
+}
+
+// Utils
+function get(selector, root = document) {
+  return root.querySelector(selector);
+}
+
+function formatDate(date) {
+  const h = "0" + date.getHours();
+  const m = "0" + date.getMinutes();
+
+  return `${h.slice(-2)}:${m.slice(-2)}`;
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
   
   
   
