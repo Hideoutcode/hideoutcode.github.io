0ac9c5f2e2bc5d50976bf3471fc632aba99f5c59
Hack3r5 <115195207+Hideoutcode@users.noreply.github.com>
2023-09-27 07:47:16 -0400

    Update proto.js

--- proto.js
+++ proto.js
@@ -1,82 +1,11 @@
- var msgerForm = get(".msger-inputarea");
-var  msgerInput = get(".msger-input");
-var  msgerChat = get(".msger-chat");
-
-
+var msgerForm = document.getElementsByClassName("msger-inputarea");
+var msgerInput = document.getElementsByClassName("msger-input");
+var msgerChat = document.getElementsByClassName("msger-chat");
 var Person_Name = "user";
- var  BOT_DIR =  "Array{ 'math' ;'code' ;'back' };";
+document.getElementsByClassName("msg-info-name").innerHTML = Person_Name;
 
-
-if( Person_Name = "user", includes("hi, hello, how are you, hola,  hallo"))  ;BOT_MSGS = [
-  "hi How are You?",
-  "hello",
-  "how can I help you?",
-  "Hi",
-  "Hi, today is a good day!",
-];}); 
-
-If( person_name = "user";includes("good", "fine", "a-okay";  BOT_MSGS = [
-  "How can I help You?",
-  "Anything I can do for you today", 
-  "I am here to help!",
-  ]));
-
-if( Person_name = "user";includes("i need", "I need", "what is", "is this",  BOT_MSGS = [ 
- "I can help you if you include key words on the subject such as math search test or joke, that is how my basic javascript ai works not that you probably care you just need a simple answer for right now I am completely irrelavant",]);
- 
-
- var array1 = Array["Math", "math","search", "test", "Joke"];
-
-If( Person_name = "user";includes("math";  BOT_DIR = [
-
- "math",
-  
-
- 
-]));
-
-
-
-
-
-
-
-
-
-
-
-
-  
-  
-  
-    
-
-
-
-
-
-
-
-
-
-// Icons made by me from unknown.org
-var  BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
-var  PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
-var  BOT_NAME = "HideoutBot";
-var  PERSON_NAME = "null";
-
-
- msgerForm.addEventListener("submit", event => {
-  event.preventDefault();
-
-  var msgText = msgerInput.value;
-  if (!msgText) return;
-
-  appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
-  msgerInput.value = "";
-
-  botResponse();
-});
+var BOT_DIR = ["math", "code", "back"];
+var BOT_MSGS = [];
 
 function appendMessage(name, img, side, text) {
   //   Simple solution for small apps
@@ -99,10 +28,60 @@ function appendMessage(name, img, side, text) {
   msgerChat.scrollTop += 500;
 }
 
+
+function checkMessage() {
+  if (Person_Name === "user" && includes("user", "hi", "hello", "how are you", "hola", "hallo")) { // I added "user" as an attempt to debug. To see if its referencing person name instead of actual message
+    BOT_MSGS = [
+      "hi How are You?",
+      "hello",
+      "how can I help you?",
+      "Hi",
+      "Hi, today is a good day!",
+    ];
+  }
+
+  if (Person_Name == "user" && includes("good", "fine", "a-okay")) {
+    BOT_MSGS = [
+      "How can I help You?",
+      "Anything I can do for you today",
+      "I am here to help!",
+    ];
+
+    if (Person_Name == "user" && includes("i need", "I need", "what is", "is this")) {
+      BOT_MSGS = ["I can help you if you include key words on the subject such as math search test or joke, that is how my basic javascript ai works not that you probably care you just need a simple answer for right now I am completely irrelavant"];
+      var array1 = Array["Math", "math", "search", "test", "Joke"];
+      if ((Person_Name = "user" && includes("math"))) {
+        BOT_DIR = ["math"];
+      }
+    }
+  }
+;
+}
+
+
+// Icons made by me from unknown.org
+var BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
+var PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
+var BOT_NAME = "HideoutBot";
+var PERSON_NAME = "null";
+
+
+msgerForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  var msgText = msgerInput.value;
+  if (!msgText) return;
+
+  appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
+  msgerInput.value = "";
+
+  botResponse();
+});
+
 function botResponse() {
   var r = random(0, BOT_MSGS.length - 1);
   var msgText = BOT_MSGS[r];
-  var  delay = msgText.split(" ").length * 100;
+  var delay = msgText.split(" ").length * 100;
 
   setTimeout(() => {
     appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
@@ -124,7 +103,3 @@ function formatDate(date) {
 function random(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
-
-  
-  
-  
